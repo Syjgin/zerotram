a5d273687323cbcb08b72c83366a7d918a279c26
Oleg Maximov <gruz103@gmail.com>
2015-08-31 21:30:04 +0300

    exit button added to records panel

---
+++ ZeroTram/Assets/Scripts/GUI/ExitButton.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ExitButton : MonoBehaviour
+{
+    [SerializeField] private Button _button;
+	// Use this for initialization
+	void Start () {
+        _button.onClick.AddListener(() => Application.LoadLevel("MainMenu"));	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
