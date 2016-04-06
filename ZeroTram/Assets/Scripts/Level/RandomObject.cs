48f2b32a42ba7b18fe89bcff965185784e9e8b65
syjgin <gruz103@gmail.com>
2016-04-06 22:18:57 +0300

    parallax fix

--- ZeroTram/Assets/Scripts/Level/RandomObject.cs
+++ ZeroTram/Assets/Scripts/Level/RandomObject.cs
@@ -6,9 +6,10 @@ using Assets.Scripts.Math;
 public class RandomObject : MonoBehaviour {
     public GameObject[] go;
     private float _time;
-    private const int MaxY = 36;
+    private const int MaxY = 50;
+    private const float YCoef = 0.1f;
     private const float YCoord = 4f;
-    private const float XCoord = 2.5f;
+    private const float XOffset = 2.5f;
     private const float ZCoord = 10;
 
     void Update ()
@@ -20,9 +21,9 @@ public class RandomObject : MonoBehaviour {
             {
                 int x = Randomizer.GetInRange(0, go.Length);
                 int y = Randomizer.GetInRange(0, MaxY);
-                Instantiate(go[x], new Vector3(XCoord + (0.1f * y), YCoord, ZCoord), transform.rotation);
-                y = Random.Range(0, 36);
-                Instantiate(go[x], new Vector3(-XCoord - (0.1f * y), YCoord, ZCoord), transform.rotation);
+                Instantiate(go[x], new Vector3(XOffset + (YCoef * y), YCoord, ZCoord), transform.rotation);
+                y = Randomizer.GetInRange(0, MaxY);
+                Instantiate(go[x], new Vector3(-XOffset - (YCoef * y), YCoord, ZCoord), transform.rotation);
                 _time = 0.2f;
             }
         }
